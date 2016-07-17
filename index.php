e7009284bab0afdc46849ffde7e85bd8bbd929fd
Brent <puddletowndesign@gmail.com>
2016-07-17 02:20:55 -0700

    Updated SCSS Structure

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/styles.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js" defer></script>
+	<script src="js/scripts.js" defer></script>
+	
+	<script src="http://localhost:8080/lib/js/live.js"></script>
+	<title></title>
+</head>
+<body>
+
+<header>
+	<h1 class="logo">
+		<a href="./" rel="home"><img src="images/logo.png" alt=""></a>
+	</h1>
+	
+	<nav>
+		<ul>
+			<li><a href="contact">Contact</a></li>
+		</ul>
+	</nav>
+</header>
+
+
+<section id="main">
+	
+</section>
+
+
+<footer>&copy;<?php echo date('Y'); ?></footer>
+</body>
+</html>
