0f2086ccfe647509aa5f9eb44035c5480679bbff
Brent <puddletowndesign@gmail.com>
2017-03-10 01:25:51 -0600

    Updated formatting

--- gulpfile.js
+++ gulpfile.js
@@ -7,53 +7,55 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
 
 // Input Sources
-var jsSources = ['dev-js/**.js'],
-    sassSources = ['css/scss/**.scss'],
+var jsSources = ['js/**.js'],
+    sassSources = ['scss/**.scss'],
     htmlSources = ['**/*.html'],
-    outputDir = 'js';
+    outputDir = './';
 
 
 gulp.task('log', function() {
-  gutil.log('== My First Task ==')
+    gutil.log('== My First Task ==');
 });
 
 gulp.task('copy', function() {
-  gulp.src('index.html')
-  .pipe(gulp.dest(outputDir))
+    gulp.src('index.html')
+        .pipe(gulp.dest(outputDir));
 });
 
 gulp.task('sass', function() {
-  gulp.src(sassSources)
-  .pipe(sass({outputStyle: 'compressed',}))
-    .on('error', gutil.log)
-  .pipe(gulp.dest('css'))
-  .pipe(connect.reload())
+    gulp.src(sassSources)
+        .pipe(sass({
+            outputStyle: 'compressed',
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('./'))
+        .pipe(connect.reload());
 });
 
 gulp.task('js', function() {
-  gulp.src(jsSources)
-  .pipe(uglify())
-  .pipe(concat('main.js'))
-  .pipe(gulp.dest(outputDir))
-  .pipe(connect.reload())
+    gulp.src(jsSources)
+        .pipe(uglify())
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(outputDir))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-  gulp.watch(jsSources, ['js']);
-  gulp.watch(sassSources, ['sass']);
-  gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch(sassSources, ['sass']);
+    gulp.watch(htmlSources, ['html']);
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    root: '.',
-    livereload: true
-  })
+    connect.server({
+        root: '.',
+        livereload: true
+    });
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
-  .pipe(connect.reload())
+    gulp.src(htmlSources)
+        .pipe(connect.reload());
 });
 
 gulp.task('default', ['html', 'js', 'sass', 'connect', 'watch']);
