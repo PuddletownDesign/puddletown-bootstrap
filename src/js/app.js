e1aa78e7f035b94e934abaeb99a1fd7d80651afd
Brent <puddletowndesign@gmail.com>
2017-10-07 23:51:09 -0500

    updated highlight display

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 import $ from 'jquery'
+
 // import utils from './calc'
 // import cleanurl from './cleanurl'
 
 /* Highlight JS Language loader */
 import hljs from 'highlight.js/lib/highlight'
 ['javascript', 'css', 'bash'].forEach((langName) => {
-  // Using require() here because import() support hasn't landed in Webpack yet
   const langModule = require(`highlight.js/lib/languages/${langName}`)
   hljs.registerLanguage(langName, langModule)
 })
